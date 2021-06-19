864a9c23c75c3357c2d050bcdce597c6fd5eb766
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-06-19 18:16:30 +0200

    fix some auth validations

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,6 +7,13 @@ import cookie from 'cookie';
 import User from '../entities/User';
 import auth from '../middleware/auth';
 
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints)[0][1];
+    return prev;
+  }, {});
+};
+
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
@@ -19,14 +26,14 @@ const register = async (req: Request, res: Response) => {
     if (emailUser) errors.email = 'Email already taken';
     if (usernameUser) errors.username = 'Username already taken';
 
-    if (Object.keys(errors).length > 0) res.status(400).json(errors);
+    if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     //create user
     const user = new User({ email, username, password });
 
     //db validation
     errors = await validate(user);
-    if (errors.length > 0) return res.status(400).json({ errors });
+    if (errors.length > 0) return res.status(400).json(mapErrors(errors));
 
     await user.save();
     return res.json(user);

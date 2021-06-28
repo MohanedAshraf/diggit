1564d3830d6b637cea8e9f0c4926aaa34a554bb6
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-06-28 23:58:03 +0200

    add vote functionality

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -42,6 +42,12 @@ export default class Comment extends Entity {
   @OneToMany(() => Vote, (vote) => vote.comment)
   votes: Vote[];
 
+  protected userVote: number;
+  setUserVote(user: User) {
+    const index = this.votes?.findIndex((v) => v.username === user.username);
+    this.userVote = index > -1 ? this.votes[index].value : 0;
+  }
+
   @BeforeInsert()
   makeIdAndSlug() {
     this.identifier = makeId(8);

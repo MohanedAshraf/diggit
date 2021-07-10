caba9aae815e79c3fc1e3190c2adcbb871078dd2
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-07-10 20:05:42 +0200

    create submit post page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -44,20 +44,18 @@ export default function Home() {
                     key={sub.name}
                     className="flex items-center px-4 py-2 text-xs border-bottom"
                   >
-                    <div className="mr-2">
-                      <Link href={`/r/${sub.name}`}>
-                        <Image
-                          src={sub.imageUrl}
-                          alt="Sub"
-                          width={(6 * 16) / 4}
-                          height={(6 * 16) / 4}
-                          className="rounded-full cursor-pointer"
-                        />
-                      </Link>
-                    </div>
+                    <Link href={`/r/${sub.name}`} passHref>
+                      <Image
+                        src={sub.imageUrl}
+                        alt="Sub"
+                        width={(6 * 16) / 4}
+                        height={(6 * 16) / 4}
+                        className="rounded-full cursor-pointer"
+                      />
+                    </Link>
 
                     <Link href={`/r/${sub.name}`}>
-                      <a className="font-bold hover:cursor-pointer">
+                      <a className="ml-2 font-bold hover:cursor-pointer">
                         /r/${sub.name}
                       </a>
                     </Link>

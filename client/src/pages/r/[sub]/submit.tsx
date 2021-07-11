ca514a9d5360aed9eae92602040e99784845c2e0
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-07-11 20:46:28 +0200

    fix submit post 'authentication'

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { FormEvent, useState } from 'react';
@@ -81,3 +82,15 @@ export default function Submit() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) throw new Error('Missing auth token cookie');
+
+    await axios.get('/auth/me', { headers: { cookie } });
+    return { props: {} };
+  } catch (err) {
+    res.writeHead(307, { Location: '/login' }).end();
+  }
+};

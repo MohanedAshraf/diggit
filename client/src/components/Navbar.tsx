5d326f1b10ee2daf2103054a23ac2d984dc5426d
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-07-12 20:17:20 +0200

    add search functionality to navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import Link from 'next/link';
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { useAuthState, useAuthDispatch } from '../context/auth';
+import Image from 'next/image';
 
 import Diggit from '../../public/images/Diggit.svg';
 import axios from 'axios';
+import { Sub } from '../types';
+import { useRouter } from 'next/router';
 
 const Navbar: React.FC = () => {
+  const [name, setName] = useState('');
+  const [subs, setSubs] = useState<Sub[]>([]);
+  const [timer, setTimer] = useState(null);
+
   const { authenticated, loading } = useAuthState();
   const dispatch = useAuthDispatch();
+
+  const router = useRouter();
 
   const logout = () => {
     axios
@@ -17,6 +26,32 @@ const Navbar: React.FC = () => {
         window.location.reload();
       })
       .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    if (name.trim() === '') {
+      setSubs([]);
+      return;
+    }
+    searchSubs();
+  }, [name]);
+  const searchSubs = async () => {
+    clearTimeout(timer);
+    setTimer(
+      setTimeout(async () => {
+        try {
+          const { data } = await axios.get(`/subs/search/${name}`);
+
+          setSubs(data);
+        } catch (err) {
+          console.log(err);
+        }
+      }, 250)
+    );
+  };
+
+  const gotoSub = (subName: string) => {
+    router.push(`/r/${subName}`);
+    setName('');
   };
 
   return (
@@ -31,13 +66,40 @@ const Navbar: React.FC = () => {
           <Link href="/ ">diggit</Link>
         </span>
       </div>
-      <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white ">
+      {/* Search  */}
+      <div className="relative flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white ">
         <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
         <input
           type="text"
           placeholder="Search"
           className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
+        <div
+          className="absolute left-0 right-0 bg-white"
+          style={{ top: '100%' }}
+        >
+          {subs?.map((sub) => (
+            <div
+              key={sub.name}
+              className="flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200"
+              onClick={() => gotoSub(sub.name)}
+            >
+              <Image
+                src={sub.imageUrl}
+                alt="Sub"
+                height={(8 * 16) / 4}
+                width={(8 * 16) / 4}
+                className="rounded-full"
+              ></Image>
+              <div className="ml-4 text-sm">
+                <p className="font-medium">{sub.name}</p>
+                <p className="text-gray-600">{sub.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="flex">

6c9ec3bbc19dabb4b3a7ade8b6b9e734a212ec94
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-07-19 12:53:43 +0200

    remove getServerSideProps from create page

--- client/src/pages/subs/create.tsx
+++ client/src/pages/subs/create.tsx
@@ -95,14 +95,14 @@ export default function Create() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  try {
-    const cookie = req.headers.cookie;
-    if (!cookie) throw new Error('Missing auth token cookie');
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   try {
+//     const cookie = req.headers.cookie;
+//     if (!cookie) throw new Error('Missing auth token cookie');
 
-    await axios.get('/auth/me', { headers: { cookie } });
-    return { props: {} };
-  } catch (err) {
-    res.writeHead(307, { Location: '/login' }).end();
-  }
-};
+//     await axios.get('/auth/me', { headers: { cookie } });
+//     return { props: {} };
+//   } catch (err) {
+//     res.writeHead(307, { Location: '/login' }).end();
+//   }
+// };

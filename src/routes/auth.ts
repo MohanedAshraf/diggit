5c83824c51591e272113cc833199e5ca32b0104a
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2021-07-19 12:31:13 +0200

    change cookie options

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -66,8 +66,8 @@ const login = async (req: Request, res: Response) => {
       'Set-Cookie',
       cookie.serialize('token', token, {
         httpOnly: true,
-        // secure: process.env.NODE_ENV == 'production',
-        sameSite: 'strict',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
+        secure: process.env.NODE_ENV === 'production', // must be true if sameSite='none'
         maxAge: 3600,
         path: '/',
       })
@@ -89,8 +89,8 @@ const logout = (_: Request, res: Response) => {
     'Set-Cookie',
     cookie.serialize('token', '', {
       httpOnly: true,
-      // secure: process.env.NODE_ENV == 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
+      secure: process.env.NODE_ENV === 'production', // must be true if sameSite='none',
       expires: new Date(0),
       path: '/',
     })

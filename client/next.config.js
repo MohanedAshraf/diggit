02b36f25c98527915f8173fbba65c5cdc4a6e9d5
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2023-02-22 23:33:43 +0200

    ignore eslint

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
+  ignoreDuringBuilds: true,
   reactStrictMode: true,
   images: {
     disableStaticImages: true,
-    domains: ['www.gravatar.com' , process.env.APP_DOMAIN]
+    domains: ["www.gravatar.com", process.env.APP_DOMAIN],
   },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      });
-  
-      return config;
-    }
-  
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
